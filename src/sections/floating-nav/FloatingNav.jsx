d49eca6cbb770481fb6e91def53a657e934d5cdb
Daniel Velnic <velnicd@yahoo.com>
2025-03-01 20:54:22 +0200

    Added the floating-nav section

---
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,9 @@
+import './floating-nav.css'
+
+const FloatingNav = () => {
+  return (
+    <section id="floating__nav"></section>
+  )
+}
+
+export default Floatin<section id="floating-nav"></section>
