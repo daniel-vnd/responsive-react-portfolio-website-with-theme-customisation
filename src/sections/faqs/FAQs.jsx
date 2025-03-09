b8e011bb1c0a8d5ef8d258a3843e312f5e40b040
Daniel Velnic <velnicd@yahoo.com>
2025-03-09 19:07:41 +0200

    Added faqs section

--- src/sections/faqs/FAQs.jsx
+++ src/sections/faqs/FAQs.jsx
@@ -1,9 +1,22 @@
 import './faqs.css'
+import faqs from './data'
+import FAQ from './FAQ'
 
 const FAQs = () => {
   return (
     <section id="faqs">
-        <h2>FAQs</h2>
+      <h2>Frequently Asked Questions</h2>
+      <p>
+        Here are some questions I usually get. 
+        Ckick to toggle  the answer, and if you still have some more questions, shoot me a message from the contact section!
+      </p>
+      <div className="container faqs__container">
+        {
+          faqs.map(faq => (
+            <FAQ key={faq.id } faq={faq}/>
+          ))
+        }
+      </div>
     </section>
   )
 }
