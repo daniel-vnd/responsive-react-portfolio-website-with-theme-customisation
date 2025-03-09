c6508db5e056df364a5cdb128901a8c2abcc37b9
Daniel Velnic <velnicd@yahoo.com>
2025-03-09 19:23:34 +0200

    Added contact section

---
+++ src/sections/contact/data.js
@@ -0,0 +1,27 @@
+import {HiOutlineMail} from "react-icons/hi"
+import {RiMessengerLine} from "react-icons/ri"
+import {FaWhatsapp} from "react-icons/fa"
+
+const data = [
+    {
+        id: 1, 
+        icon: <HiOutlineMail />, 
+        link: 'mailto:dummygator@gmail.com'
+    },
+    {
+        id: 2, 
+        icon: <RiMessengerLine />, 
+        link: 'http://www.messenger.com/1/10008721788007'
+    },
+    {
+        id: 3, 
+        icon: <FaWhatsapp />, 
+        link: 'https://api.whatsapp.com/send?phone=%2B233557097546'
+    }
+];
+
+// alternative whatsApp link
+// https://wa.me/yournumber
+
+
+export default data;
